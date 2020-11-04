2d80d9b82163f5389dab290c7c16741d178b31e4
Alexandre Bruyelles <git@jack.fr.eu.org>
2020-11-04 18:08:04 +0100

    use transmission_rpc
    Signed-off-by: Alexandre Bruyelles <git@jack.fr.eu.org>

--- commands/dl.py
+++ commands/dl.py
@@ -1,12 +1,8 @@
-import sh
+from transmission_rpc import Client
 from discord.ext import commands
 
 import rarbgapi
 from config import *
-
-tremote = sh.Command('transmission-remote').bake(transmission_url)
-if transmission_auth is not None:
-	tremote = tremote.bake('--auth', transmission_auth)
 
 class Command(commands.Cog, name='dl'):
 	def __init__(self, bot):
@@ -18,4 +14,8 @@ class Command(commands.Cog, name='dl'):
 		rarbg = rarbgapi.RarbgAPI()
 		i = rarbg.search(search_string=torrent, sort='last')[0]
 		await ctx.send(f'{i.filename} ({i.category}) has been scheduled for download')
-		tremote('-a', i.download)
+		if transmission_auth is not None:
+			c = Client(path='/torrent', username=transmission_auth[0], password=transmission_auth[1])
+		else:
+			c = Client(path='/torrent')
+		c.add_torrent(i.download)
--- config.py
+++ config.py
@@ -4,4 +4,4 @@ transmission_url = 'http://localhost:9091/torrent'
 
 # If transmission is protected via basic auth, fill in the login:password
 # Else, set it to None
-transmission_auth = 'user:passwd'
+transmission_auth = ('user', 'passwd')
